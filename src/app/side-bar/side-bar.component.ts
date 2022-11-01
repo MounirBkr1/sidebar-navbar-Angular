56acaa22a500429b25f14eb921bc78683a55cb73
Mounir Bkr <miirbri1@gmail.com>
2022-11-01 17:16:27 +0100

    commit 0 "sidebar & navbar"

---
+++ src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import $ from "jquery";
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  // @ts-ignore
+  ngOnInit(): void {
+
+
+  }
+
+}
